971946fcc843bcfdf35f99dcefcc397205c9935d
Marcos <marcosallanm1to@gmail.com>
2024-02-22 14:56:11 -0300

    Configurando disponibilidade - Input e output do Zod

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -1,3 +1,4 @@
+import { zodResolver } from '@hookform/resolvers/zod'
 import {
   Button,
   Checkbox,
@@ -6,20 +7,62 @@ import {
   Text,
   TextInput,
 } from '@ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+import { Controller, useFieldArray, useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { getWeekDays } from '../../../utils/get-week-days'
 import { Container, Header } from '../styles'
+
 import {
+  FormError,
   IntervalBox,
+  IntervalContainer,
   IntervalDay,
   IntervalInputs,
   IntervalItem,
-  IntervalsContainer,
 } from './styles'
-import { ArrowRight } from 'phosphor-react'
-import { z } from 'zod'
-import { Controller, useFieldArray, useForm } from 'react-hook-form'
-import { getWeekDays } from '@/src/utils/get-week-days'
+import { convertTimeStringToMinutes } from '@/src/utils/convert-time-string-to-minutes'
 
-const timeIntervalsFormSchema = z.object({})
+const timeIntervalsFormSchema = z.object({
+  intervals: z
+    .array(
+      z.object({
+        weekDay: z.number().min(0).max(6),
+        enabled: z.boolean(),
+        startTime: z.string(),
+        endTime: z.string(),
+      }),
+    )
+    .length(7)
+    .transform((intervals) => intervals.filter((interval) => interval.enabled))
+    .refine((intervals) => intervals.length > 0, {
+      message: 'Você precisa selecionar pelo menos um dia da semana!',
+    })
+    .transform((intervals) => {
+      return intervals.map((interval) => {
+        return {
+          weekDay: interval.weekDay,
+          startTimeInMinutes: convertTimeStringToMinutes(interval.startTime),
+          endTimeInMinutes: convertTimeStringToMinutes(interval.endTime),
+        }
+      })
+    })
+    .refine(
+      (intervals) => {
+        return intervals.every(
+          (interval) =>
+            interval.endTimeInMinutes - 60 >= interval.startTimeInMinutes,
+        )
+      },
+      {
+        message:
+          'O horário de término deve ser pelo menos 1h distante do início.',
+      },
+    ),
+})
+
+type TimeIntervalsFormInput = z.input<typeof timeIntervalsFormSchema>
+type TimeIntervalsFormOutput = z.output<typeof timeIntervalsFormSchema>
 
 export default function TimeIntervals() {
   const {
@@ -28,7 +71,8 @@ export default function TimeIntervals() {
     control,
     watch,
     formState: { isSubmitting, errors },
-  } = useForm({
+  } = useForm<TimeIntervalsFormInput>({
+    resolver: zodResolver(timeIntervalsFormSchema),
     defaultValues: {
       intervals: [
         { weekDay: 0, enabled: false, startTime: '08:00', endTime: '18:00' },
@@ -44,18 +88,25 @@ export default function TimeIntervals() {
 
   const weekDays = getWeekDays()
 
-  const { fields } = useFieldArray({ control, name: 'intervals' })
+  const { fields } = useFieldArray({
+    control,
+    name: 'intervals',
+  })
 
   const intervals = watch('intervals')
 
-  async function handleSetTimeIntervals() {}
+  async function handleSetTimeIntervals(data: any) {
+    const formData = data as TimeIntervalsFormOutput
+
+    console.log(formData)
+  }
 
   return (
     <Container>
       <Header>
-        <Heading as="strong">Quase lá!</Heading>
+        <Heading as="strong">Quase lá</Heading>
         <Text>
-          Defina o intervalo de horários que você está disponível em cada dia da
+          Defina o intervalo de horário que você está disponível em cada dia da
           semana.
         </Text>
 
@@ -63,7 +114,7 @@ export default function TimeIntervals() {
       </Header>
 
       <IntervalBox as="form" onSubmit={handleSubmit(handleSetTimeIntervals)}>
-        <IntervalsContainer>
+        <IntervalContainer>
           {fields.map((field, index) => {
             return (
               <IntervalItem key={field.id}>
@@ -74,9 +125,9 @@ export default function TimeIntervals() {
                     render={({ field }) => {
                       return (
                         <Checkbox
-                          onCheckedChange={(checked) => {
+                          onCheckedChange={(checked) =>
                             field.onChange(checked === true)
-                          }}
+                          }
                           checked={field.value}
                         />
                       )
@@ -89,25 +140,29 @@ export default function TimeIntervals() {
                     size="sm"
                     type="time"
                     step={60}
-                    {...register(`intervals.${index}.startTime`)}
                     disabled={intervals[index].enabled === false}
+                    {...register(`intervals.${index}.startTime`)}
                     crossOrigin={undefined}
                   />
                   <TextInput
                     size="sm"
                     type="time"
                     step={60}
-                    {...register(`intervals.${index}.endTime`)}
                     disabled={intervals[index].enabled === false}
+                    {...register(`intervals.${index}.endTime`)}
                     crossOrigin={undefined}
                   />
                 </IntervalInputs>
               </IntervalItem>
             )
           })}
-        </IntervalsContainer>
+        </IntervalContainer>
 
-        <Button type="submit">
+        {errors.intervals && (
+          <FormError size="sm">{errors.intervals.root?.message}</FormError>
+        )}
+
+        <Button type="submit" disabled={isSubmitting}>
           Próximo passo
           <ArrowRight />
         </Button>
--- src/pages/register/time-intervals/styles.ts
+++ src/pages/register/time-intervals/styles.ts
@@ -1,4 +1,4 @@
-import { Box, styled } from '@ignite-ui/react'
+import { Box, styled, Text } from '@ignite-ui/react'
 
 export const IntervalBox = styled(Box, {
   marginTop: '$6',
@@ -6,7 +6,7 @@ export const IntervalBox = styled(Box, {
   flexDirection: 'column',
 })
 
-export const IntervalsContainer = styled('div', {
+export const IntervalContainer = styled('div', {
   border: '1px solid $gray600',
   borderRadius: '$md',
   marginBottom: '$4',
@@ -37,4 +37,9 @@ export const IntervalInputs = styled('div', {
   'input::-webkit-calendar-picker-indicator': {
     filter: 'invert(100%) brightness(40%)',
   },
+})
+
+export const FormError = styled(Text, {
+  color: '#f75a68',
+  marginBottom: '$4',
 })
